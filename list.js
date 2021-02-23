90b7bcfbda44003898d901a43f2b729d434fe72d
Duane McDonald <mcdonald.duane13@gmail.com>
2021-02-23 10:08:47 -0500

    review/refactor started
    removed JS from html, broke apart giant function, using Seperation of Concerns, building proper SRP functions.

--- list.js
+++ list.js
@@ -1,47 +1,53 @@
-// creates element of the to-do item
-function newItem() {
-    var li = document.createElement("li");
-    var checkbox = document.createElement("input");
-    checkbox.type = "checkbox";
-    checkbox.className = "checkbox";
-    checkbox.onclick = checkOffTask;
-    var inputValue = document.getElementById("myInput").value;
-    var newItemName = document.createTextNode(inputValue);
-    var dueBy = document.createElement("span")
-    dueBy.innerText = "Due By: "
-    dueBy.className = "dueBy"
-    var dateValue = document.getElementById("myDate").value;
-    var dueDate = document.createElement("input");
-    dueDate.type = "date";
-    dueDate.className = "dueDate";
-    dueDate.defaultValue = dateValue;
-    var closeBtn = document.createElement("button");
-    var fire = document.createTextNode(" \uD83D\uDD25");
-    closeBtn.className = "close";
-    closeBtn.append(fire);
-    closeBtn.onclick = deleteTask;
-// localstorage & Creates UI task with all the pieces, if not blank 
-    li.append(checkbox,"   ",newItemName," ",dueBy,dueDate,"  ",closeBtn);
-    if (inputValue === '') { alert("Enter a task");}
+//window.onload = savedTodos();
+
+function createTodo() {
+    const inputValue = document.getElementById("myInput").value;
+    if (inputValue === '') { 
+        alert("Enter a task");
+        return;
+    }
     else {
+    }
+//create todo completed toggle
+function createCheckbox() {
+const checkbox = document.createElement("input");
+checkbox.type = "checkbox";
+checkbox.className = "checkbox";
+checkbox.onclick = checkOffTask;
+}
+//create due date
+function create() {}
+const dueBy = document.createElement("span")
+dueBy.innerText = "Due By: "
+dueBy.className = "dueBy"
+const dateValue = document.getElementById("myDate").value;
+const dueDate = document.createElement("input");
+dueDate.type = "date";
+dueDate.className = "dueDate";
+dueDate.defaultValue = dateValue;
+
+// create remove button
+function create() {}
+const closeBtn = document.createElement("button");
+const fire = document.createTextNode(" \uD83D\uDD25");
+closeBtn.className = "close";
+closeBtn.append(fire);
+closeBtn.onclick = deleteTask;
+
+//Create
+function create() {}
+const newTodo = document.createElement("li");
+const newItemName = document.createTextNode(inputValue);
+newTodo.append(checkbox,newItemName,dueBy,dueDate,closeBtn);
+
         // new item appended
-        document.getElementById("todoList").appendChild(li);
-        // update and save list
-        var todoList = [];
-        current_todoList = document.getElementById("todoList");
-        listItems = current_todoList.innerHTML;
-        todoList += listItems;
-        localStorage.setItem("saved", (JSON.stringify(todoList)));
-    } 
+        document.getElementById("todoList").appendChild(newTodo);
+        
 //reset inputs
     document.getElementById("myInput").value = "";
     document.getElementById("myDate").value = "";
-}
-//load previous tasks
-function savedTasks(){
-    savedTasks = JSON.parse(localStorage.getItem("saved"));
-    document.getElementById("todoList").innerHTML = savedTasks;
-}
+
+
 //Reset App
 function clearAll(){
     localStorage.clear();
@@ -49,17 +55,33 @@ function clearAll(){
 }
 // fire button functionality
 function deleteTask(){
-    var div = this.parentElement;
+    const div = this.parentElement;
     div.remove();
 }
 // task completed functionality, with un-complete option
 function checkOffTask(){
-    var div = this.parentElement;
+    const div = this.parentElement;
     div.className += "completed";
     this.onclick = uncheckTask;
     }
 function uncheckTask(){
-    var div = this.parentElement;
+    const div = this.parentElement;
     div.className = "";
     this.onclick = checkOffTask;
 }
+
+/*
+// update and save list
+const todoList = [];
+current_todoList = document.getElementById("todoList");
+listItems = current_todoList.innerHTML;
+todoList += listItems;
+localStorage.setItem("saved", (JSON.stringify(todoList)));
+} 
+
+//load previous tasks
+function savedTodos(){
+    savedTasks = JSON.parse(localStorage.getItem("saved"));
+    document.getElementById("todoList").innerHTML = savedTasks;
+}
+/*
