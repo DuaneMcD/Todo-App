3441f6ecf120e78e6e11e4a9e24467333be04bbb
Duane McDonald <mcdonald.duane13@gmail.com>
2021-02-23 11:03:33 -0500

    refactoring logic via code review
    naming conventions addressed for variables(const) and functions, .onclick changed to addEventListener(), SOC append new todo li from css

--- list.js
+++ list.js
@@ -1,22 +1,45 @@
-//window.onload = savedTodos();
+const todo = createTodo();
 
-function createTodo() {
-    const inputValue = document.getElementById("myInput").value;
-    if (inputValue === '') { 
+function createTodo(text) {
+    const input = document.getElementById("myInput");
+    const inputValue = input.value;
+    if (input === '') { 
         alert("Enter a task");
         return;
     }
     else {
+        const todoListItem = document.createElement("li");
+        const todoText = createTodoParagraph(text);
+        const toggleTodoCheckBox = createToggleTodoCheckbox(todoText);
+        const deleteButton = createDeleteButton(todoListItem);
+        todoListItem.append(toggleTodoCheckBox);
+        todoListItem.append(todoText);
+        todoListItem.append(deleteButton);
+
+        // new item appended
+        document.getElementById("todoList").append(newTodo);
+        
+        input.value = "";
+        dateInput.value = "";
     }
+function createTodoParagraph(text) {
+    const todoParagraph = document.createElement("p");
+    todoParagraph.textContent = text;
+    return todoParagraph;
+
+}
 //create todo completed toggle
-function createCheckbox() {
-const checkbox = document.createElement("input");
-checkbox.type = "checkbox";
-checkbox.className = "checkbox";
-checkbox.onclick = checkOffTask;
+function createToggleTodoCheckbox(todoText) {
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.className = "checkbox";
+    checkbox.addEventListener{"click", () => (
+        toggleCheckedOff(todText)
+})
+    return checkbox;
 }
 //create due date
-function create() {}
+function createDisplayDueDate() {}
 const dueBy = document.createElement("span")
 dueBy.innerText = "Due By: "
 dueBy.className = "dueBy"
@@ -27,50 +50,31 @@ dueDate.className = "dueDate";
 dueDate.defaultValue = dateValue;
 
 // create remove button
-function create() {}
-const closeBtn = document.createElement("button");
-const fire = document.createTextNode(" \uD83D\uDD25");
-closeBtn.className = "close";
-closeBtn.append(fire);
-closeBtn.onclick = deleteTask;
+function createDeleteButton() {}
+    const deleteButton = document.createElement("button");
+    const fire = document.createTextNode(" \uD83D\uDD25");
+    deleteButton.className = "delete";
+    deleteButton.append(fire);
+    deleteButton.onclick = deleteTask;
+    return deleteButton;
 
-//Create
-function create() {}
-const newTodo = document.createElement("li");
-const newItemName = document.createTextNode(inputValue);
-newTodo.append(checkbox,newItemName,dueBy,dueDate,closeBtn);
-
-        // new item appended
-        document.getElementById("todoList").appendChild(newTodo);
-        
-//reset inputs
-    document.getElementById("myInput").value = "";
-    document.getElementById("myDate").value = "";
-
-
+function toggleCheckedOff(todoText){
+    todoText.classList.toggle("completed");
+}
+function deleteTask(todoListItem){
+    remove();
+}
 //Reset App
 function clearAll(){
     localStorage.clear();
     window.location.reload();
 }
-// fire button functionality
-function deleteTask(){
-    const div = this.parentElement;
-    div.remove();
-}
-// task completed functionality, with un-complete option
-function checkOffTask(){
-    const div = this.parentElement;
-    div.className += "completed";
-    this.onclick = uncheckTask;
-    }
-function uncheckTask(){
-    const div = this.parentElement;
-    div.className = "";
-    this.onclick = checkOffTask;
-}
+
+
 
 /*
+//window.onload = savedTodos();
+
 // update and save list
 const todoList = [];
 current_todoList = document.getElementById("todoList");
